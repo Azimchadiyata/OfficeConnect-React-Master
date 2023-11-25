e40652957c22b4189bd0ff9ef5922a17da5ef14a
Azim Chadiyata <104197073+Azimchadiyata@users.noreply.github.com>
2023-11-25 14:14:58 +0530

    .

---
+++ src/app/App.jsx
@@ -0,0 +1,28 @@
+import { CssBaseline } from '@mui/material';
+import { useRoutes } from 'react-router-dom';
+import { MatxTheme } from './components';
+import { AuthProvider } from './contexts/JWTAuthContext';
+import { SettingsProvider } from './contexts/SettingsContext';
+import routes from './routes';
+import '../fake-db';
+import { APIbackendProvider } from 'APIbackendurl';
+
+const App = () => {
+  const content = useRoutes(routes);
+
+  return (
+    <APIbackendProvider>
+      <SettingsProvider>
+        <AuthProvider>
+          <MatxTheme>
+            <CssBaseline />
+            {content}
+          </MatxTheme>
+        </AuthProvider>
+      </SettingsProvider>
+    </APIbackendProvider>
+
+  );
+};
+
+export default App;
