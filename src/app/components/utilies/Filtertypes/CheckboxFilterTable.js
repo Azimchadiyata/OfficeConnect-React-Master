f2169257bb24f77312aa50cb09040055ccc0e622
Azim Chadiyata <104197073+Azimchadiyata@users.noreply.github.com>
2023-11-25 15:46:17 +0530

    .

---
+++ src/app/components/utilies/Filtertypes/CheckboxFilterTable.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Checkbox } from "primereact/checkbox";
+const CheckboxFilterTable = () => {
+  const [projectchecked, setProjectchecked] = useState(false);
+  return (
+    <div className="col-md-4 mt-3 ">
+      <div className="field-checkbox m-1 row">
+        <Checkbox
+          inputId="binary"
+          checked={projectchecked}
+          onChange={(e) => setProjectchecked(e.checked)}
+        />
+        <label htmlFor="binary" className="text-xs mt-1">
+          Project Connect User
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default CheckboxFilterTable;
